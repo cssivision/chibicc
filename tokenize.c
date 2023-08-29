15f7e9252772f9cc4c3349f7f9d496ff068af262
cssivision <cssivision@gmail.com>
2023-08-29 19:22:47 +0800

    Add line and block comments

--- tokenize.c
+++ tokenize.c
@@ -257,6 +257,27 @@ Token *tokenize(char *path, char *p)
 
     while (*p)
     {
+        // Skip line comments.
+        if (startswith(p, "//"))
+        {
+            p += 2;
+            while (*p != '\n')
+                p++;
+            continue;
+        }
+
+        // Skip block comments.
+        if (startswith(p, "/*"))
+        {
+            char *q = strstr(p + 2, "*/");
+            if (!q)
+            {
+                error_at(p, "unclosed block comment");
+            }
+            p = q + 2;
+            continue;
+        }
+
         if (isspace(*p))
         {
             p++;
