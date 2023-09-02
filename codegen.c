62433cf963b9ea5e4e52f602bba137324bc89d90
cssivision <cssivision@gmail.com>
2023-09-02 04:14:24 +0000

    Add struct assignment

--- codegen.c
+++ codegen.c
@@ -34,7 +34,7 @@ static void pop(char *arg)
 // Load a value from where %rax is pointing to.
 static void load(Type *ty)
 {
-    if (ty->kind == TY_ARRAY)
+    if (ty->kind == TY_ARRAY || ty->kind == TY_STRUCT || ty->kind == TY_UNION)
     {
         // If it is an array, do not attempt to load a value to the
         // register because in general we can't load an entire array to a
@@ -59,6 +59,16 @@ static void load(Type *ty)
 static void store(Type *ty)
 {
     pop("%rdi");
+
+    if (ty->kind == TY_STRUCT || ty->kind == TY_UNION)
+    {
+        for (int i = 0; i < ty->size; i++)
+        {
+            println("  mov %d(%%rax), %%r8b", i);
+            println("  mov %%r8b, %d(%%rdi)", i);
+        }
+        return;
+    }
 
     if (ty->size == 1)
     {
