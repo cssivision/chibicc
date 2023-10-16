972b24b928b0c5b586ece658abfa902494744735
cssivision <cssivision@gmail.com>
2023-10-16 19:37:34 +0800

    Ignore "static" and "const" in array-dimensions

--- parse.c
+++ parse.c
@@ -1732,9 +1732,14 @@ static Type *func_params(Token **rest, Token *tok, Type *ty)
     return ty;
 }
 
-// array-dimensions = num? "]" type-suffix
+// array-dimensions = ("static" | "restrict")*  const-expr? "]" type-suffix
 static Type *array_dimensions(Token **rest, Token *tok, Type *ty)
 {
+    while (equal(tok, "static") || equal(tok, "restrict"))
+    {
+        tok = tok->next;
+    }
+
     if (equal(tok, "]"))
     {
         tok = tok->next;
