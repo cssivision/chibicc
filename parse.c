c55706c78fb513467db3e6b1e376be1c7074c1bf
cssivision <cssivision@gmail.com>
2023-10-20 11:15:13 +0800

    Add "long double" as an alias for "double"

--- parse.c
+++ parse.c
@@ -1755,6 +1755,7 @@ static Type *declspec(Token **rest, Token *tok, VarAttr *attr)
             ty = ty_float;
             break;
         case DOUBLE:
+        case LONG + DOUBLE:
             ty = ty_double;
             break;
         default:
