f8f7475164db6ea248dc4f3dc240ca252284d285
cssivision <cssivision@gmail.com>
2023-11-17 13:08:07 +0800

    Add defined() macro operator

--- preprocess.c
+++ preprocess.c
@@ -24,8 +24,6 @@
 
 #include "chibicc.h"
 
-static Token *preprocess2(Token *tok);
-
 typedef struct MacroParam MacroParam;
 struct MacroParam
 {
@@ -77,6 +75,9 @@ struct CondIncl
 };
 
 static CondIncl *cond_incl;
+
+static Token *preprocess2(Token *tok);
+static Macro *find_macro(Token *tok);
 
 static bool is_hash(Token *tok)
 {
@@ -194,11 +195,52 @@ static Token *copy_line(Token **rest, Token *tok)
     return head.next;
 }
 
+static Token *new_num_token(int val, Token *tmpl)
+{
+    char *buf = format("%d\n", val);
+    return tokenize(new_file(tmpl->file->name, tmpl->file->file_no, buf));
+}
+
+static Token *read_const_expr(Token **rest, Token *tok)
+{
+    tok = copy_line(rest, tok);
+    Token head = {};
+    Token *cur = &head;
+
+    while (tok->kind != TK_EOF)
+    {
+        if (equal(tok, "defined"))
+        {
+            Token *start = tok;
+            bool has_paren = consume(&tok, tok->next, "(");
+
+            if (tok->kind != TK_IDENT)
+            {
+                error_tok(start, "macro name must be an identifier");
+            }
+            Macro *m = find_macro(tok);
+            tok = tok->next;
+
+            if (has_paren)
+            {
+                tok = skip(tok, ")");
+            }
+
+            cur = cur->next = new_num_token(m ? 1 : 0, start);
+            continue;
+        }
+
+        cur = cur->next = tok;
+        tok = tok->next;
+    }
+    return head.next;
+}
+
 // Read and evaluate a constant expression.
 static long eval_const_expr(Token **rest, Token *tok)
 {
     Token *start = tok;
-    Token *expr = copy_line(rest, tok->next);
+    Token *expr = read_const_expr(rest, tok->next);
 
     expr = preprocess2(expr);
 
