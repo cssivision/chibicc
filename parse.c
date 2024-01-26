90bbc4f42c5a19ec1441c9fdb7f0f240975eeaa7
cssivision <cssivision@gmail.com>
2024-01-26 15:37:34 +0800

    Add _Generic

--- parse.c
+++ parse.c
@@ -369,11 +369,66 @@ static Obj *new_string_literal(char *str, Type *ty)
     return var;
 }
 
+// generic-selection = "(" assign "," generic-assoc ("," generic-assoc)* ")"
+//
+// generic-assoc = type-name ":" assign
+//               | "default" ":" assign
+static Node *generic_selection(Token **rest, Token *tok)
+{
+    Token *start = tok;
+    tok = skip(tok, "(");
+
+    Node *ctrl = assign(&tok, tok);
+    add_type(ctrl);
+
+    Type *t1 = ctrl->ty;
+    if (t1->kind == TY_FUNC)
+    {
+        t1 = pointer_to(t1);
+    }
+    else if (t1->kind == TY_ARRAY)
+    {
+        t1 = pointer_to(t1->base);
+    }
+
+    Node *ret = NULL;
+    while (!consume(rest, tok, ")"))
+    {
+        tok = skip(tok, ",");
+        if (equal(tok, "default"))
+        {
+            tok = skip(tok->next, ":");
+            Node *node = assign(&tok, tok);
+            if (!ret)
+            {
+                ret = node;
+            }
+            continue;
+        }
+
+        Type *t2 = typename(&tok, tok);
+        tok = skip(tok, ":");
+        Node *node = assign(&tok, tok);
+        if (is_compatible(t1, t2))
+        {
+            ret = node;
+        }
+    }
+
+    if (!ret)
+    {
+        error_tok(start, "controlling expression type not compatible with"
+                         " any generic association type");
+    }
+    return ret;
+}
+
 // primary = "(" expr ")"
 //          | ("{" stmt+ "}")
 //          | ident
 //          | num
 //          | sizeof unary
+//          | "_Generic" generic-selection
 //          | "__builtin_types_compatible_p" "(" type-name, type-name, ")"
 //          | "_Alignof" "(" type-name ")"
 //          | "_Alignof" unary
@@ -456,6 +511,11 @@ static Node *primary(Token **rest, Token *tok)
         node->ty = tok->ty;
         *rest = tok->next;
         return node;
+    }
+
+    if (equal(tok, "_Generic"))
+    {
+        return generic_selection(rest, tok->next);
     }
 
     if (equal(tok, "__builtin_types_compatible_p"))
